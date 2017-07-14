9409a61d0c0e70ae9bc6e7ea6d6d8d610bd5d1ee
Rofaeil Ashaiaa <rofaeilashaiaa@gmail.com>
2017-07-14 11:06:30 +0200

    added the creative sdk

---
+++ app/src/main/java/rofaeil/ashaiaa/idea/cameraapp/MyApplication.java
@@ -0,0 +1,43 @@
+package rofaeil.ashaiaa.idea.cameraapp;
+
+import android.app.Application;
+
+import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
+import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
+
+import rofaeil.ashaiaa.idea.cameraapp.util.Keys;
+
+
+public class MyApplication extends Application implements IAdobeAuthClientCredentials {
+
+    private static final String CREATIVE_SDK_CLIENT_ID = Keys.CSDK_CLIENT_ID;
+    private static final String CREATIVE_SDK_CLIENT_SECRET = Keys.CSDK_CLIENT_SECRET;
+    private static final String CREATIVE_SDK_REDIRECT_URI = Keys.CSDK_REDIRECT_URI;
+    private static final String[] CREATIVE_SDK_SCOPES = Keys.CSDK_SCOPES;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AdobeCSDKFoundation.initializeCSDKFoundation(getApplicationContext());
+    }
+
+    @Override
+    public String getClientID() {
+        return CREATIVE_SDK_CLIENT_ID;
+    }
+
+    @Override
+    public String getClientSecret() {
+        return CREATIVE_SDK_CLIENT_SECRET;
+    }
+
+    @Override
+    public String[] getAdditionalScopesList() {
+        return CREATIVE_SDK_SCOPES;
+    }
+
+    @Override
+    public String getRedirectURI() {
+        return CREATIVE_SDK_REDIRECT_URI;
+    }
+}
