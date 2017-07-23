ba6548f0b853943116e938e166087189238b41a9
Rofaeil Ashaiaa <rofaeilashaiaa@gmail.com>
2017-07-23 12:28:34 +0200

    added second widget to display last taken photo

---
+++ app/src/main/java/rofaeil/ashaiaa/idea/cameraapp/widget/LastCapturedImageAppWidget.java
@@ -0,0 +1,66 @@
+package rofaeil.ashaiaa.idea.cameraapp.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.widget.RemoteViews;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+import rofaeil.ashaiaa.idea.cameraapp.R;
+
+import static rofaeil.ashaiaa.idea.cameraapp.data.local.database.ImagesContract.ImagesEntry.COLUMN_IMAGE_PATH;
+import static rofaeil.ashaiaa.idea.cameraapp.data.local.database.ImagesContract.ImagesEntry.CONTENT_URI;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class LastCapturedImageAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.last_captured_image_app_widget);
+
+        final long identityToken = Binder.clearCallingIdentity();
+
+        Cursor cursor = context.getContentResolver().query(CONTENT_URI, null, null, null,null );
+
+        Binder.restoreCallingIdentity(identityToken);
+
+        if (cursor != null){
+
+            cursor.moveToLast();
+            String lastCapturedImagePath = cursor.getString(cursor.getColumnIndex(COLUMN_IMAGE_PATH));
+            views.setImageViewUri(R.id.appwidget_photo , Uri.parse(lastCapturedImagePath));
+        }
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
